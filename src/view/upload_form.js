4d68e874467afe3b8bc05f0c22e5a3fcab68acf2
Tricktionary <andyaoux@hotmail.com>
2019-09-14 20:57:39 -0400

    Creating custom value

--- src/view/upload_form.js
+++ src/view/upload_form.js
@@ -11,37 +11,45 @@ const pageSection = {
     width: '80%'
 }
 
+
 const SUBMIT_FORM = gql`
-            mutation{
-                createSong( name:"test", pageRangeStart:1, pageRangeEnd:3, pageCount:4){
-                    song{
-                        name
-                    }
+        mutation{
+            createSong( name:"test", pageRangeStart:1, pageRangeEnd:3, pageCount:4){
+                song{
+                    name
                 }
             }
-        `;
+        }
+`;
+
 
 export function UploadForm(){
     let input;
     const [uploadMutation, { data }] = useMutation(SUBMIT_FORM);
 
+    let handleSubmit = function(e){
+        e.preventDefault();
+        console.log(input.value);
+        uploadMutation(
+            { 
+                variables: {
+                    type: input.value 
+            } 
+        })
+        input.value = '';
+        ;
+    }
     return (
-        <div>
-          <form
-            onSubmit={e => {
-              console.log("TEST");
-              e.preventDefault();
-              uploadMutation({ variables: { type: input.value } });
-              input.value = '';
-            }}
-          >
-            <input
-              ref={node => {
-                input = node;
-              }}
-            />
-            <button className="ui button" type="submit">Add Todo</button>
-          </form>
+        <div style={pageSection}>
+            <h1>Song Upload Form</h1>
+            <form className="ui form" onSubmit={handleSubmit}>
+                <div className="field">
+                    <label>Song Name</label>
+                    <input type="text" placeholder="Song Name" ref={node => { input = node; }}/>
+                </div>
+                
+                <button className="ui button green" type="submit">Add Todo</button>
+            </form>
         </div>
       );
     
