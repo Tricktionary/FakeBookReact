e1e0c91ae61b7d615758404485d892fbbd0ab966
Tricktionary <andyaoux@hotmail.com>
2019-09-16 17:39:14 -0400

    Upload form work

--- src/view/upload_form.js
+++ src/view/upload_form.js
@@ -1,43 +1,44 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import gql from 'graphql-tag';
 import { useMutation } from '@apollo/react-hooks';
 
-const marginTop = {
-    marginTop: '20px'
-}
 const pageSection = {
     marginLeft: '10%',
     marginTop: '5%',
     width: '80%'
 }
 
-
 const SUBMIT_FORM = gql`
-        mutation{
-            createSong( name:"test", pageRangeStart:1, pageRangeEnd:3, pageCount:4){
-                song{
-                    name
-                }
+    mutation createSong($name: String!, $pageRangeStart: Int!, $pageRangeEnd:Int!,$pageCount:Int! ){
+        createSong(name:$name, pageRangeStart: $pageRangeStart ,pageRangeEnd: $pageRangeEnd,pageCount:$pageCount){
+            song{
+                name
             }
         }
+    }
 `;
 
-
 export function UploadForm(){
-    let input;
-    const [uploadMutation, { data }] = useMutation(SUBMIT_FORM);
+    const [name, setName] = useState();
+    const [pageRangeStart, setPageRangeStart] = useState();
+    const [pageRangeEnd, setPageRangeEnd] = useState();
+    const [pageCount, setPageCount] = useState();
+
+    const [uploadMutation] = useMutation(SUBMIT_FORM);
+
 
     let handleSubmit = function(e){
         e.preventDefault();
-        console.log(input.value);
-        uploadMutation(
-            { 
-                variables: {
-                    type: input.value 
+ 
+        uploadMutation({ 
+            variables: {
+                name: name, 
+                pageRangeStart: parseInt(pageRangeStart),
+                pageRangeEnd: parseInt(pageRangeEnd),
+                pageCount: parseInt(pageCount)
             } 
-        })
-        input.value = '';
-        ;
+        });
+        
     }
     return (
         <div style={pageSection}>
@@ -45,10 +46,36 @@ export function UploadForm(){
             <form className="ui form" onSubmit={handleSubmit}>
                 <div className="field">
                     <label>Song Name</label>
-                    <input type="text" placeholder="Song Name" ref={node => { input = node; }}/>
+                    <input type="text" placeholder="Song Name" onChange={function(event){
+                        setName(event.target.value);
+                    }}/>
+                </div>
+
+                <div className="field"> 
+                    <label>Page Range Start</label>                    
+                    <input type="number" placeholder="0" onChange={function(event){
+                        setPageRangeStart(event.target.value);
+                    }}/>
                 </div>
                 
-                <button className="ui button green" type="submit">Add Todo</button>
+                <div className="field"> 
+                    <label>Page Range End</label>
+                    <input type="number" placeholder="0" onChange={function(event){
+                        setPageRangeEnd(event.target.value);
+                    }}/>
+                </div>
+
+                <div className="field"> 
+                    <label>Total Range Count</label>
+                    <input type="number" placeholder="0" onChange={function(event){
+                        setPageCount(event.target.value);
+                    }}/>
+                </div>
+                
+                <div className="field"> 
+                    <button className="ui button green" type="submit">Add Todo</button>
+                </div>
+
             </form>
         </div>
       );
